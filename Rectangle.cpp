6fabd7c081904fd7ec5027339a9e371e3413ae30
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2016-04-28 21:15:37 +0200

    Update and rename Rectangle to Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,42 +1,23 @@
 #include <iostream>
 #include "operaciones_rectangulo.h"
-
 using namespace std;
 
 int main()
-
 {
-
 unsigned short i=0;
-
 cout << "¿Cuantos rectangulos quieres calcular?";
-
 cin >> i;
-
 Rectangulo r;
-
 for(unsigned short c=0; c<i; c++)
-
 {
-
 cout << endl << "¿Cual es la base del rectangulo " << c+1 << "? ";
-
 cin >> r.ancho;
-
 cout << "¿Cual es la altura del rectangulo " << c+1 << "? ";
-
 cin >> r.alto;
-
 cout << endl << "El area del rectangulo " << c+1 << " es " <<
-
 calcula_area(r) << endl;
-
 cout << "El perimetro del rectangulo " << c+1 << " es " <<
-
 calcula_perimetro(r) << endl;
-
 }
-
 return 0;
-
 }
