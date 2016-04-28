2ed6b16c776f05fca2045f1f3e72aab5a3adf5b4
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2016-04-28 21:35:49 +0200

    Example use Operators

--- Operator.cpp
+++ Operator.cpp
@@ -1,3 +1,20 @@
+class Polinomio {
+// polinomio cout:= a[n-1]x^(n-1) + ... + a[0]
+public:
+Polinomio();
+Polinomio(int g, double *c);
+Polinomio(double c);
+void SetValue(int i, double c);
+void SetValue(int g, double *c);
+Polinomio operator +(const Polinomio& p);
+double operator [](double x);
+friend std::istream& operator>> (std::istream& in, Polinomio& p);
+friend std::ostream& operator<< (std::ostream& out, const Polinomio& p);
+private:
+int grado;
+double *coef;
+};
+
 Polinomio Polinomio::operator +(const Polinomio& p){
 double *c;
 if(this->grado>p.grado){
@@ -17,3 +34,14 @@ c[i] = p.coef[i];
 return Polinomio(p.grado, c);
 }
 }
+
+double Polinomio::operator [](double x){
+double eval = this->coef[0];
+double cof;
+for (int i = 1; i <= grado; i++){
+cof = this->coef[i];
+for(int j = 0; j < i; j++)
+cof = cof*x;
+eval += cof;
+}
+return eval;
